abba3d28bf551cd7cf02873fd071e91bd685c778
Anurag Kumar <anuragkumar@Anurags-MacBook-Pro.local>
2026-09-25 17:56:02 +0530

    updated main and other files like gitignore and toml file

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,9 +1,12 @@
+from fastapi.testclient import TestClient
 
-from xmlrpc import client
+from app.main import app
+
+client = TestClient(app)
 
 
 def test_health_check():
     response = client.get("/health/")
-    
-    assert ...
-    assert ...
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
