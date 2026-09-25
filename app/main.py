c6fc19a204ff9401d3160482efe03361beb2083b
Anurag Kumar <anuragkumar@Anurags-MacBook-Pro.local>
2026-09-25 17:05:39 +0530

    added test health and other files too

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from app import health
+from tests import test_health
 
 app = FastAPI()
 app.include_router(health.router, prefix="/health", tags=["Health"])
+app.include_router(test_health.router, prefix="/test_health", tags=["Test Health"])
